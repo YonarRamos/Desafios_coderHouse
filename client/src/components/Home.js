5dbad668af4640ca2608b13534b64d86f7b03caa
unknown <yonar.ramos@systelec.com>
2021-10-09 09:49:42 -0300

    finixng front register and login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './Home.css'
 
 const Home = ( props ) => {
     console.log(props)
     return (
-        <div className="card text-center">
+        <div className="card text-center bienvenida">
             <div className="card-body">
                 <h3 className="card-title">HOLA { props.usario }</h3>
                 <p className="card-text">Bienvenido a nuestro ecommerce</p>
--- client/src/components/Registrar.jsx
+++ client/src/components/Registrar.jsx
@@ -2,16 +2,33 @@ import React, { useState } from 'react';
 import axios from '../utils/axios'
 import './Registrar.css'
 
-function Register() {
+function Registrar() {
 
     const [msgServer, setMsgServer] = useState('');
-
+    const [user, setUser] = useState({
+        email:'',
+        password:'',
+        nombre:'',
+        apellido:'',
+        edad:'',
+        alias:'',
+        avatar:''
+    });
+    const signUp = (e)=>{
         try {
-            axios.post('usuarios/register', {
-                //user
-            })
+            axios.post('usuarios/registrar', user)
             .then((res)=>{
                 console.log('Usuario registrado', res);
+                alert(`Usuario ${user.nombre} agregado correctamente!!`)
+                setUser({
+                    email:'',
+                    password:'',
+                    nombre:'',
+                    apellido:'',
+                    edad:'',
+                    alias:'',
+                    avatar:''
+                })
             })
             .catch((error)=>{
                 if(error.response){
@@ -25,51 +42,50 @@ function Register() {
         } catch (error) {
             console.error('LOGIN ERRROR', error);
         }
+    }
 
     return (
         <div className="container container__usuario">
             <h5 className="agregar__usuario">Nuevo Usuario</h5>
-            <form>
                 <div className="mb-3">
                     <label htmlFor="nombre" className="form-label">Nombre:</label>
-                    <input type="text" className="form-control" id="nombre" aria-describedby="nombre"/>
+                    <input value={ user.nombre } onChange= { e => setUser({ ...user, nombre : e.target.value })} type="text" className="form-control" id="nombre" aria-describedby="nombre"/>
                     <div id="nombreHelp" className="form-text"></div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email:</label>
-                    <input type="text" className="form-control" id="email" aria-describedby="email"/>
-                    <div id="emailHelp" className="form-text"></div>
-                </div>
-                <div className="mb-3">
                     <label htmlFor="apellido" className="form-label">Apellido:</label>
-                    <input type="text" className="form-control" id="apellido" aria-describedby="apellido"/>
+                    <input value= { user.apellido } onChange= { e => setUser({ ...user, apellido : e.target.value })} type="text" className="form-control" id="apellido" aria-describedby="apellido"/>
                     <div id="apellidoHelp" className="form-text"></div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="edad" className="form-label">Edad:</label>
-                    <input type="number" className="form-control" id="edad" aria-describedby="edad"/>
+                    <input value={ user.edad } onChange= { e => setUser({ ...user, edad : e.target.value })} type="number" className="form-control" id="edad" aria-describedby="edad"/>
                     <div id="edadHelp" className="form-text"></div>
                 </div>  
                 <div className="mb-3">
                     <label htmlFor="alias" className="form-label">Alias:</label>
-                    <input type="text" className="form-control" id="alias" aria-describedby="alias"/>
+                    <input value= { user.alias } onChange= { e => setUser({ ...user, alias : e.target.value })} type="text" className="form-control" id="alias" aria-describedby="alias"/>
                     <div id="aliasHelp" className="form-text"></div>
                 </div> 
                 <div className="mb-3">
                     <label htmlFor="avatar" className="form-label">avatar:</label>
-                    <input type="text" className="form-control" id="avatar" aria-describedby="avatar"/>
+                    <input value={ user.avatar } onChange= { e => setUser({ ...user, avatar : e.target.value })} type="text" className="form-control" id="avatar" aria-describedby="avatar"/>
                     <div id="avatarHelp" className="form-text"></div>
                 </div>
                 <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email:</label>
+                    <input value={ user.email } onChange= { e => setUser({ ...user, email : e.target.value })} type="text" className="form-control" id="email" aria-describedby="email"/>
+                    <div id="emailHelp" className="form-text"></div>
+                </div>
+                <div className="mb-3">
                     <label htmlFor="paswword" className="form-label">Password:</label>
-                    <input type="password" className="form-control" id="password" aria-describedby="password"/>
+                    <input value={ user.password } onChange= { e => setUser({ ...user, password : e.target.value })} type="password" className="form-control" id="password" aria-describedby="password"/>
                     <div id="passwordHelp" className="form-text"></div>
                 </div>         
-                <div>{msgServer}</div>     
-                <button type="submit" className="btn btn-primary">AGREGAR</button>
-            </form>
+                <div className="msg__server">{ msgServer }</div>     
+            <button type="button" onClick={signUp} className="btn btn-primary">Enviar</button>
         </div>
     );
 }
 
-export default Register;
+export default Registrar;
